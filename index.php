6b63a3bcaacbeba16e04a26b89a970b5fcc1fca6
Roylin <dereklobo89@gmail.com>
2018-09-12 17:57:01 -0400

    loading spinner

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
       #result {
         display: none;
       }
+      .error {
+        border: 1px solid red;
+      }
+      #spinner {
+        display: none;
+      }
     </style>
   </head>
   <body>
@@ -25,6 +31,10 @@
       </form>
     </div>
 
+    <div id="spinner">
+      <img src="spinner.gif" width="50" height="50" />
+    </div>
+
     <div id="result">
       <p>The total volume is: <span id="volume"></span></p>
     </div>
@@ -33,6 +43,45 @@
 
       var result_div = document.getElementById("result");
       var volume = document.getElementById("volume");
+      var button = document.getElementById("ajax-submit");
+      var orig_button_value = button.value;
+
+      function showSpinner() {
+        var spinner = document.getElementById("spinner");
+        spinner.style.display = 'block';
+      }
+
+      function hideSpinner() {
+        var spinner = document.getElementById("spinner");
+        spinner.style.display = 'none';
+      }
+
+      function disableSubmitButton() {
+        button.disabled = true;
+        button.value = 'Loading...';
+      }
+
+      function enableSubmitButton() {
+        button.disabled = false;
+        button.value = orig_button_value;
+      }
+
+      function displayErrors(errors) {
+        var inputs = document.getElementsByTagName('input');
+        for(i=0; i < inputs.length; i++) {
+          var input = inputs[i];
+          if(errors.indexOf(input.name) >= 0) {
+            input.classList.add('error');
+          }
+        }
+      }
+
+      function clearErrors() {
+        var inputs = document.getElementsByTagName('input');
+        for(i=0; i < inputs.length; i++) {
+          inputs[i].classList.remove('error');
+        }
+      }
 
       function postResult(value) {
         volume.innerHTML = value;
@@ -44,11 +93,12 @@
         result_div.style.display = 'none';
       }
 
-      function gatherFormData(form){
+      // omits textareas, select-options, checkboxes, radio buttons
+      function gatherFormData(form) {
         var inputs = form.getElementsByTagName('input');
         var array = [];
-        for(i =0;i<inputs.length;i++){
-          var inputNameValue = inputs[i].name + "=" + inputs[i].value;
+        for(i=0; i < inputs.length; i++) {
+          var inputNameValue = inputs[i].name + '=' + inputs[i].value;
           array.push(inputNameValue);
         }
         return array.join('&');
@@ -56,37 +106,43 @@
 
       function calculateMeasurements() {
         clearResult();
+        clearErrors();
+        showSpinner();
+        disableSubmitButton();
 
         var form = document.getElementById("measurement-form");
         var action = form.getAttribute("action");
 
-        var form_data = new FormData(form);
-        for([key,value] of form_data.entries()){
-          console.log(key+ ":" + value);
-        }
-
-       
-
-
-        // determine form action
         // gather form data
+        var form_data = new FormData(form);
+        for ([key, value] of form_data.entries()) {
+          console.log(key + ': ' + value);
+        }
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', action, true);
+        // do not set content-type with FormData
         //xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.onreadystatechange = function () {
           if(xhr.readyState == 4 && xhr.status == 200) {
             var result = xhr.responseText;
             console.log('Result: ' + result);
-            var json  = JSON.parse(result);
-            postResult(json.volume);
+
+            hideSpinner();
+            enableSubmitButton();
+
+            var json = JSON.parse(result);
+            if(json.hasOwnProperty('errors') && json.errors.length > 0) {
+              displayErrors(json.errors);
+            } else {
+              postResult(json.volume);
+            }
           }
         };
         xhr.send(form_data);
       }
 
-      var button = document.getElementById("ajax-submit");
       button.addEventListener("click", calculateMeasurements);
 
     </script>
